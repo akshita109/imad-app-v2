7d3431b9c32e8099be6c19e091cda1adb95325f4
akshita109 <akshitagarg10@gmail.com>
2017-06-14 11:03:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,22 @@
-console.log('Loaded!');
-
-var img=document.getElement8yID('madi');
-img.onclick = function()
+//counter code
+var button = document.getElementById("counter");
+button.onclick = function()
 {
-    img.style.marginLeft='100px';
+    //create a request object
+    var request = newXMLHttpRequest();
+    //capture the response
+    requst.onreadystatechange == function(){
+         if(request.readystate == XMLHttpRequest.DONE){
+             //take action
+             if (request.status==200){
+                  var counter = request.responseText;
+                  var span = document.getElementById("count");
+                  span.innerHTML = counter.toString();
+             }
+         }
+         //take no action.wait.
+    };
+    //make the request
+    request.open('GET','http://akshita109.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
-
